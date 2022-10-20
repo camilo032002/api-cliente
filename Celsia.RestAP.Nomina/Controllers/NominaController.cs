271b5c9e11ce9cf09a7d0359e82a299df76bfc2f
Juan Camilo Lopez Damian <jclopezd@celsia.com>
2022-10-20 10:24:34 -0500

    se agrega el proyecto

---
+++ Celsia.RestAP.Nomina/Controllers/NominaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Celsia.RestAP.Nomina.Controllers
+{
+    public class NominaController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Echoping()
+        {
+            return Ok(true);
+        }
+    }
+}
---
+++ Celsia.RestAP.Nomina/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Celsia.RestAP.Nomina.WebApiApplication" Language="C#" %>
